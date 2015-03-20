93b6959914d447806b71784b21fe49a817961895
Jeff Nelson <LongShot902@gmail.com>
2015-03-20 14:51:43 -0500

    Updating Initial Commit
    Updating Initial Commit

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+	function getData($server) {
+		$ssh = new Net_SSH2($server);
+		$key = new Crypt_RSA();
+		$key->loadKey(file_get_contents('./private.key'));
+		if (!$ssh->login('root', $key)) {
+			return 'Login Failed or Key does not exist.';
+		}
+		else {
+			$chunks = explode("\n", $ssh->exec('ls /dev/shm/worker-data'));
+			array_pop($chunks);
+			
+			$total_files = count($chunks);
+			$cores = $ssh->exec('nproc');
+			
+			if ($total_files == $cores + 7) {
+				array_unshift($chunks, "Working");
+			}
+			elseif ($total_files < $cores + 7 && $total_files + 7 > 7) {
+				array_unshift($chunks, "Cooling Down");
+			}
+			else {
+				array_unshift($chunks, "Idle");
+			}
+			
+			array_unshift($chunks, $cores);
+			return $chunks;
+		}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	include('Net/SSH2.php');
+	include('Crypt/RSA.php');
+	include('functions.php');
+?>
+<html>
+<head>
+	<title>Monitor</title>
+	<meta http-equiv="refresh" content="15">
+</head>
+<body>
+	<font size="6"><b>Monitor Status</b></font><br />
+	<font size="2">[Refreshing every 15 seconds...]</font><br /><br />
+	
+	<table border="1">
+		<tr><?php
+				$gs01 = getData('10.10.0.11');
+				$gs02 = getData('10.10.0.12');
+				$gs03 = getData('10.10.0.13');
+			?>
+			<td>GS01 - <?php echo $gs01[1]; ?></td><td>GS02 - <?php echo $gs01[1]; ?></td><td>GS03 - <?php echo $gs01[1]; ?></td>
+		</tr>
+		<tr>
+			<td valign="top"><pre>
+			<?php print_r(array_slice($gs01,2,count($gs01))); ?>
+			</pre></td>
+			<td valign="top"><pre>
+			<?php print_r(array_slice($gs02,2,count($gs02))); ?>
+			</pre></td>
+			<td valign="top"><pre>
+			<?php print_r(array_slice($gs03,2,count($gs03))); ?>
+			</pre></td>
+		</tr>
+</body>
+</html>
